c5bd0fcba7b0ca2a7908e2782124d7242c8b82d5
Edita Marija <52567746+EditaMarija@users.noreply.github.com>
2022-10-14 09:29:40 +0200

    Initial commit

---
+++ TodoListApp/TodoListApp/TodoListViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace TodoListApp
+{
+    class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+            TodoItems.Add(new TodoItem("walk the dog", true));
+            TodoItems.Add(new TodoItem("wash dishes", false));
+            TodoItems.Add(new TodoItem("get more coffee", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        public void AddTodoItem()
+        {  
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+            //NewTodoInputValue = string.Empty;
+            NewTodoInputValue = null;
+            //NewTodoInputValue = "TEST";       
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }      
+
+    }
+}
